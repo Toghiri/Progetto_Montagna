ebf52a3e0692a4938b215c3b77f9bd6dfd21921a
Pietro <33959286+Toghiri@users.noreply.github.com>
2023-09-20 10:15:38 +0200

    feat: added enemy

---
+++ scripts/orso.gd
@@ -0,0 +1,20 @@
+extends Node2D
+const GameOverScreen = preload("res://nodes/game_over_screen.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_orso_body_entered(body):
+	if body.is_in_group("player"):
+		var game_over = GameOverScreen.instantiate()
+		add_child(game_over)
+		print("Sei morto fra")
+		get_tree().paused = true
