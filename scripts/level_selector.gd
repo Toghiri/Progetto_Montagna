f4558e538163476a3d5492c13649b6cfa395bdcc
Pietro <33959286+Toghiri@users.noreply.github.com>
2023-09-21 23:54:30 +0200

    feat: added timer, command list, panorama level

--- scripts/level_selector.gd
+++ scripts/level_selector.gd
@@ -21,3 +21,7 @@ func _on_lvl_2_pressed():
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://nodes/main_menu.tscn")
+
+
+func _on_panorama_pressed():
+	get_tree().change_scene_to_file("res://nodes/panorama.tscn")
